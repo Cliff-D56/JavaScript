6a6d3527304a502dc269e99d758a2ebbf05603c1
Cliff-D56 <cliffordgardner9@gmail.com>
2025-01-30 01:55:10 -0600

    1/30/2025

---
+++ JS_Essentials_2/Module_3/Built_In_Objects.js
@@ -0,0 +1,7 @@
+// Built in Objects are prebuilt objects with usable functionalities
+let n = new Number(2662); // Number is a prebuilt object
+
+// Some prebuilt objects have static and regular methods, ex: Number has both
+
+console.log(n.toFixed(2)) // Regular method
+console.log(Number.isInteger(n)) // Static method
