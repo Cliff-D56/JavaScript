23986f426c832e8aa92ecf104908f86142758dbb
Cliff-D56 <cliffordgardner9@gmail.com>
2025-01-31 16:27:55 -0600

    1/31/2025

--- Binary_Coverter.js
+++ Binary_Coverter.js
@@ -3,8 +3,8 @@ let numChoice = prompt("Type in a number: ");
 
 // Checks if choice is a number
 while(isNaN(numChoice)){
-    numChoice = prompt("Please use an actual number")
-}
+    numChoice = prompt("Please use an actual number");
+};
 
 // Starting byte size
 let byteSize = 1;
@@ -17,39 +17,57 @@ function converter(number){
     // Creates bit array
     for (i =0;i<bits;i++){
         array.push(2**i)
-    }
-    bitSum = array.reduce((x,y)=>{return x+y})
+    };
+    bitSum = array.reduce((x,y)=>{return x+y});
     
     // Readability variables
-    let placeHolder = bitSum.toString().length
-    let nplaceHolder = numChoice.length
+    let placeHolder = bitSum.toString().length;
+    let nplaceHolder = numChoice.length;
     let numStringChoice= numChoice;
     
     // Subtracts bits from number choice
     for(i=0;i<array.length;i++){
         // If more than one byte, adds space in between for readability
-        if(i%8===0&&i!==0){
+        if(i%4===0&&i!==0){
             binary.push(" ")
-        }
+        };
         // Current bit
         let currentBit = array.length - (i+1);
-        let currentValue = array[currentBit]
+        let currentValue = array[currentBit];
         // Subtracts current bit value from number choice and populates a 1 or 0 for binary conversion
         number>=currentValue?(number-=currentValue,binary.push(1)):binary.push(0); 
-    }
+    };
 
     // Adding Commas for readability
     while(placeHolder > 3){
         bitSum= bitSum.toString().slice(0,placeHolder-3)+","+bitSum.toString().slice(placeHolder-3);
         placeHolder-=3;
-    }
+    };
     while(nplaceHolder > 3){
         numStringChoice= numStringChoice.slice(0,nplaceHolder-3)+","+numStringChoice.slice(nplaceHolder-3);
         nplaceHolder-=3;
-    }
+    };
    
-    // If there is any remainder of the number, byte size increases and the converted runs all over again with more space
+    // If there is any remainder of the number, byte size increases and the converter runs all over again with more space
+    number>0?(byteSize++,console.log(`increasing byte size to ${byteSize}, ${numStringChoice} is over ${bitSum}`),converter(numChoice))
+    
     // If no remainder, combines binary conversion into readable string with spaces in between
-    number>0?(byteSize++,console.log(`increasing byte, ${numStringChoice} is over ${bitSum}`),converter(numChoice)):console.log(binary.join(""))
+    :console.log(`Byte size is ${byteSize}\nThe number: ${numStringChoice}\nIn binary: ${binary.join("")}`);
 }
 converter(numChoice)
+
+
+
+// let num = parseInt(numChoice)
+// let len = (num).toString(2).length-1;
+// let string = (num).toString(2);
+// let answer = "";
+// for(let i = len;i>3;i--){
+//     i%4==0&&i!=0?answer+=(string[i]+" "):answer+=string[i];
+// }
+// if(len<3){
+//     console.log("HI")
+//     answer+="0"
+// }
+// answer+=string.substring(len-3)
+// console.log(answer)
---
+++ JS_Essentials_2/Module_3/S1_Simple_Data_Types.js
@@ -0,0 +1,18 @@
+/* Most Simple Data Types
+        >Boolean
+        >Number
+        >String
+        >Date
+*/
+
+// Boolean -> True or False values
+let bool = new Boolean(); // Creates new Boolean object
+let bool2 = Boolean(1); // Evaluates if input is true or not, not a new object
+let bool3 = true; bool3.valueOf() // booleans can be auto-boxed
+
+// Number -> Numberical values, can include decimals 
+let num = new Number(); // Creates new Boolean object
+let num2 = Number(5); // DON'T DO THIS, ASSIGNS value to variable
+let num3 = 5; // DO THIS
+
+// String -> Word or sentence values, string of characters
