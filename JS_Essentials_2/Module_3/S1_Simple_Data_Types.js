34d7968e9fc6bcc8f4a5e1b7234c8cb47254bf37
Cliff-D56 <cliffordgardner9@gmail.com>
2025-02-18 20:41:50 -0600

    Simple Data Types

--- JS_Essentials_2/Module_3/S1_Simple_Data_Types.js
+++ JS_Essentials_2/Module_3/S1_Simple_Data_Types.js
@@ -16,3 +16,14 @@ let num2 = Number(5); // DON'T DO THIS, ASSIGNS value to variable
 let num3 = 5; // DO THIS
 
 // String -> Word or sentence values, string of characters
+let string = new String(); // Creates new String object
+let string2 = String('Hello World'); // Evaluates if input is a string or not, not a new object
+let string3 = 'Hello World'; // DO THIS
+
+// Date -> Date and time values
+let date = new Date(); // Creates new Date object, current time and date
+let date1 = new Date(2020, 6); // July 2020, 2nd argument is month
+let date2 = new Date(2020, 6, 8); // July 8, 2020. 3rd argument is day
+let date3 = new Date(2020, 6, 8, 10); // July 8, 2020, 10:00, 4th argument is hour
+let date4 = new Date(2020, 6, 8, 10, 20, 45); // July 8, 2020, 10:20:45, 5th argument is minute, 6th is second
+
--- Test.js
+++ Test.js
@@ -48,15 +48,15 @@ let student1 = new User({name: 'Rafael', surname: 'Fife', email: 'rfife@rhyta.co
 let student2 = new User({name: 'Kelly', surname: 'Estes', email: 'k_estes@dayrep.com', role: 'student'});
 let teacher1 = new User({name: 'Paula', surname: 'Thompkins', email: 'PaulaThompkins@jourrapide.com', role: 'teacher'});
 
-student1.addCourse('maths', 2);
-student1.addCourse('physics', 1);
-student1.removeCourse('physics');
-teacher1.addCourse('biology', 3);
-teacher1.editCourse('biology', 4);
-console.log(`${student1.name}: ${student1.courses} courses`); // -> Rafael: 1 courses
-console.log(`${teacher1.name}: ${teacher1.courses} courses`); // -> Paula: 1 courses
-teacher1.sendMessage(student1, 'test message');
-teacher1.sendMessage(student1, 'another message');
-teacher1.showMessagesHistory();
-// -> rfife@rhyta.com -> PaulaThompkins@jourrapide.com: test message
-// -> rfife@rhyta.com -> PaulaThompkins@jourrapide.com: another message
+// student1.addCourse('maths', 2);
+// student1.addCourse('physics', 1);
+// student1.removeCourse('physics');
+// teacher1.addCourse('biology', 3);
+// teacher1.editCourse('biology', 4);
+// console.log(`${student1.name}: ${student1.courses} courses`); // -> Rafael: 1 courses
+// console.log(`${teacher1.name}: ${teacher1.courses} courses`); // -> Paula: 1 courses
+// teacher1.sendMessage(student1, 'test message');
+// teacher1.sendMessage(student1, 'another message');
+// teacher1.showMessagesHistory();
+
+  
