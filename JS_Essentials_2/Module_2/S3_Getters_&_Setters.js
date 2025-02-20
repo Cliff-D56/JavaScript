27619c55e8da0fa34eee2da7edbaaa96cc594f48
Cliff-D56 <cliffordgardner9@gmail.com>
2025-02-19 23:55:29 -0600

    update

--- JS_Essentials_2/Module_2/S3_Getters_&_Setters.js
+++ JS_Essentials_2/Module_2/S3_Getters_&_Setters.js
@@ -37,4 +37,5 @@ class Vehicle {
 let vehicle = new Vehicle({longitude: 18.213423, latitude: 59.367628, id: "AL1024"});
 vehicle.position = {longitude: 18.193121, latitude: 59.378654};
 console.log(vehicle.position) //returns properties without being called like function
+vehicle.setPosition({longitude:5,latitude:5}) 
 console.log(vehicle.getPosition()) 
