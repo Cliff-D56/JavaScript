2fd33a805bf85c9348674600a8eaf6c6f439b3c2
Cliff-D56 <cliffordgardner9@gmail.com>
2025-02-25 23:41:40 -0600

    update

--- JS_Essentials_2/Module_3/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-for(let item of collection3){
-//     if (collection1 instanceof Map){
-//         console.log(item[0], item[1]);
-//     }
-//     else{
-//         console.log(item);
-//     }
-// }
