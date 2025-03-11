69b0229264c36f9a774920473fcd18b8397f8150
Cliff-D56 <cliffordgardner9@gmail.com>
2025-03-10 19:12:23 -0500

    Async notes

--- JS_Essentials_2/Module_4/S3_Asynchronous_Programming.js
+++ JS_Essentials_2/Module_4/S3_Asynchronous_Programming.js
@@ -8,5 +8,13 @@
     let function2 = function(){
         console.log("End")
     }
+    
+    // Asynchronous functions, are functions that happen at their own pace regardless of others
+    let afunc1 = function(message,time){
+        let fn = function(){console.log(message)}
+        setTimeout(fn,time)
+    }
+
+    afunc1("Started before the Synchronous function but ended after, no function needs to wait for this one to finish",3000)
     function1(function2)
 }
